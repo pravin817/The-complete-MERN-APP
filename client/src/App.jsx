d8b95c8c3792dccdb1145d0550503b5bed78d700
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-02-18 09:23:52 +0530

    The react-router-dom configured successfully.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import All components
+import UserName from "./components/UserName";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<UserName />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/password" element={<Password />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/recovery" element={<Recovery />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
